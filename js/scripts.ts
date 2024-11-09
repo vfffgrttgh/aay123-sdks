0b879f9c68d81ca7a711267da48af7afdae9a0af
vfffgrttgh <163544061+vfffgrttgh@users.noreply.github.com>
2024-11-09 21:38:42 +0530

    Create scripts.ts

---
+++ js/scripts.ts
@@ -0,0 +1,11 @@
+// ALERTS
+
+const message: string = "Whatever you want";
+
+alert(message); // TRIGGER ON EVENTS OR INSIDE FUNCTIONS, YOUR CHOICE.
+
+// CONFIRMATIONS
+
+const msg: string = "ARE YOU SURE?";
+
+confirm(msg); // SAME.
