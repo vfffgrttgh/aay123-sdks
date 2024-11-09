0cb794897c6dbaffcd9224d53f2228cdabdb894b
vfffgrttgh <163544061+vfffgrttgh@users.noreply.github.com>
2024-11-09 21:06:30 +0530

    Create Caching.vue

---
+++ vue/Backend/Caching.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export default {
+    methods: {
+      addItem({name, value}: any): void {
+        localStorage.setItem(name, value);
+      },
+
+      getItem(name: any): void {
+        localStorage.getItem(name);
+      },
+
+      deleteItem(name: any): void {
+        localStorage.deleteItem(name);
+      },
+    },
+  }
+</script>
