e593594834d5af41e25c9b15d0c8ff1ce8ebfe64
vfffgrttgh <163544061+vfffgrttgh@users.noreply.github.com>
2024-11-09 21:27:23 +0530

    Create Caching.svelte

---
+++ svelte/Backend/Caching.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  addItem({name, value}: any): void {
+    localStorage.setItem(name, value);
+  },
+
+  getItem(name: any): void {
+    localStorage.getItem(name);
+  },
+
+  deleteItem(name: any): void {
+    localStorage.deleteItem(name);
+  },
+};
+</script>
