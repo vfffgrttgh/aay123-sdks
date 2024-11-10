bcc30866c78cabffdc89d922f5db25d1ad1bd0e2
vfffgrttgh <163544061+vfffgrttgh@users.noreply.github.com>
2024-11-10 08:23:53 +0530

    Update auth.ts

--- js/auth.ts
+++ js/auth.ts
@@ -1,1 +1,13 @@
-// AUTH
+const usernameInput: HTMLInputElement = document.getElementById("username");
+const passwordInput: HTMLInputElement = document.getElementById("password");
+const btn: any = document.getElementById("btn");
+
+function handleSubmit(): void {
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  localStorage.setItem("Username", username);
+  localStorage.setItem("Password", password);
+};
+
+btn.onclick = handleSubmit();
